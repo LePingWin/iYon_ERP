b841451bb59009077eb4d469c5be4da7e8c19ac1
Laurent Freyss <lfreyss@titanium.fr>
2018-11-06 12:19:03 +0100

    add project repo

--- iYon_ERP/Repositories/ProjectRepository.cs
+++ iYon_ERP/Repositories/ProjectRepository.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using iYon_ERP.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace iYon_ERP.Repositories
 {
-    class ProjectRepository
+    public class ProjectRepository
     {
+        private List<Project> listProjects { get; set; }
+        private string fileUrl = ConfigurationManager.AppSettings["projectsFileUrl"];
+        public List<Project> GetAllItems()
+        {
+            using (StreamReader file = File.OpenText(@"c:\movie.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                listProjects = (List<Project>)serializer.Deserialize(file, typeof(Project));
+            }
+            return listProjects;
+        }
     }
 }
