7daa3d58357110abf39f4c3b96813bcb1cea7a32
LePingWin <loic.bine@gmail.com>
2018-11-20 12:24:56 +0100

    Describing future method

--- iYon_ERP/Controllers/MainController.cs
+++ iYon_ERP/Controllers/MainController.cs
@@ -30,5 +30,12 @@ namespace iYon_ERP.Controllers
 
             return mainVM;
         }
+
+        //Méthode : On affecte tout le monde au même projet, on vérifie la date de fin de chaque tâche (ici dev et pilotage). 
+        //La date la plus tardive représente la fin du projet. 
+        //L'autre date indique qu'on peut affecter un développeur à un projet à une certaine date
+        //On affecte le prochain
+        //TODO : Fonction qui affecte un projet à
+        //TODO : Fonction d'ordonancement de projets
     }
 }
