6f88574aef52b46c366b3b4156b934877a0fbc85
Laurent Freyss <lfreyss@titanium.fr>
2018-12-04 11:25:02 +0100

    add wrapper'

---
+++ iYon_ERP/Models/ProjectWrapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iYon_ERP.Models
+{
+    public class ProjectWrapper
+    {
+        public Project project { get; set; }
+        public DateTime DevsEndDate{ get; set; }
+        public DateTime ProjectManagementEndDate { get; set; }
+
+        public DateTime GetProjectEndDate {
+            get
+            {
+                return DevsEndDate <= ProjectManagementEndDate ? DevsEndDate : ProjectManagementEndDate;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Project : " + project.Name + " | DevsTimeEnd : " + DevsEndDate.ToShortDateString() + " | ProjectManagementEnd : " + ProjectManagementEndDate.ToShortDateString() + "\n";
+        }
+
+        public ProjectWrapper(Project project)
+        {
+            this.project = project;
+        }
+
+        public int GetProjectBiggestWorkLoad()
+        {
+            return this.project.GetWorkLoad(Type.Developer) < this.project.GetWorkLoad(Type.ProjectManager) ? this.project.GetWorkLoad(Type.Developer) : this.project.GetWorkLoad(Type.ProjectManager);
+        }
+    }
+}
